8e6e5d5f6798e6611daf3fbf32c14887fa2af4af
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-07-09 12:03:01 +0700

    [Bài tập] Access modifier

--- src/TestCircle.java
+++ src/TestCircle.java
@@ -1,6 +1,6 @@
 public class TestCircle {
     public static void main(String[] args) {
-        Circle circle = new Circle(10);
+        Circle circle = new Circle(Math.random()*100);
         System.out.println(circle.getArea());
     }
 }
